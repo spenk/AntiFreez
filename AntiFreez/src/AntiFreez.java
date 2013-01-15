47f3b77132ea3184c6f7b0d698fb30e1650c7d8d
spenk <spenko@hotmail.nl>
2013-01-15 22:52:52 +0100

    Update AntiFreez/src/AntiFreez.java
    cleanup and update to 1.2

--- AntiFreez/src/AntiFreez.java
+++ AntiFreez/src/AntiFreez.java
@@ -1,27 +1,27 @@
 import java.io.File;
 import java.util.logging.Logger;
 
-public class AntiFreez extends Plugin
-{
-	  String name = "AntiFreez";
-	  String version = "1.0";
-	  String author = " spenk";
-	  static Logger log = Logger.getLogger("Minecraft");
-	  
-	  
-public void initialize(){
-	AntiFreezListener listener = new AntiFreezListener();
-log.info(this.name +" version "+ this.version + " by " +this.author+(" is initialized!"));
-etc.getLoader().addListener(PluginLoader.Hook.BLOCK_UPDATE, listener, this, PluginListener.Priority.MEDIUM);
-File f = new File("plugins/config");
-f.mkdir();
-listener.reload();
-}
-public void enable(){
-	log.info(this.name + " version " + this.version + " by " + this.author + " is enabled!");
-}
+public class AntiFreez extends Plugin {
+	String name = "AntiFreez";
+	String version = "1.2";
+	String author = " spenk";
+	static Logger log = Logger.getLogger("Minecraft");
 
-public void disable(){
-	log.info(this.name + " version " + this.version + " is disabled!");
-}
+	public void initialize() {
+		AntiFreezListener listener = new AntiFreezListener();
+		log.info(this.name + " version " + this.version + " by " + this.author+ " is initialized!");
+		etc.getLoader().addListener(PluginLoader.Hook.BLOCK_UPDATE, listener,this, PluginListener.Priority.MEDIUM);
+		File f = new File("plugins/config");
+		f.mkdir();
+		listener.reload();
+	}
+
+	public void enable() {
+		log.info(this.name + " version " + this.version + " by " + this.author
+				+ " is enabled!");
+	}
+
+	public void disable() {
+		log.info(this.name + " version " + this.version + " is disabled!");
+	}
 }
