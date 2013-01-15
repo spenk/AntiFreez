ac688948652f40614339a293f07b4cb64d797813
spenk <spenko@hotmail.nl>
2013-01-15 22:53:17 +0100

    Update AntiFreez/src/AntiFreezListener.java
    cleanup and updated to 1.2

--- AntiFreez/src/AntiFreezListener.java
+++ AntiFreez/src/AntiFreezListener.java
@@ -1,29 +1,31 @@
-public class AntiFreezListener extends PluginListener{
+public class AntiFreezListener extends PluginListener {
 	boolean icemelt;
 	boolean frost;
-	PropertiesFile props = new PropertiesFile("plugins/config/AntiFreez.properties");
-	public void reload(){
-		icemelt = props.getBoolean("Allow-Ice-Melt", false);
-		frost = props.getBoolean("Allow-Water-To-Ice", false);
+	boolean snowmelt;
+	PropertiesFile props = new PropertiesFile(
+			"plugins/config/AntiFreez.properties");
+
+	public void reload() {
+		this.icemelt = this.props.getBoolean("DisAllow-(ice)Melt", false);
+		this.snowmelt = this.props.getBoolean("DisAllow-(snow)Melt", false);
+		this.frost = this.props.getBoolean("DisAllow-Frost", false);
 	}
-	
-	public boolean onBlockUpdate(Block block){
-		
-	    if (block.getType() == 79){
-	    	if (!icemelt){
-	    		return true;
-	    		
-	    	}else{
-	    		return false;
-	    	}
-	    	} 
-	    if (block.getType() == 9){
-	    	if (!frost){
-	    		return true;
-	    	}else{
-	    		return false;
-	    	}
-	    }
+
+	public boolean onBlockUpdate(Block Oblock, int newBlockId) {
+		switch (Oblock.getType()) {
+		case 79:
+			if (newBlockId == 8 || newBlockId == 9) {
+				return this.icemelt;
+			}
+		case 9:
+			if (newBlockId == 79) {
+				return this.frost;
+			}
+		case 78:
+			if (newBlockId == 0) {
+				return this.snowmelt;
+			}
+		}
 		return false;
-	    }
-	  }
+	}
+}
